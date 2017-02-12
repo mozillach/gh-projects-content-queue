b3eabc14bf703baa4586de2d9dddfaadf7373246
Martin Giger <martin@humanoids.be>
2017-02-12 18:15:09 +0100

    Fully test getTweetIDFromUrl

--- test/twitter-account.js
+++ test/twitter-account.js
@@ -2,16 +2,40 @@ import test from 'ava';
 import TwitterAccount from '../lib/twitter-account';
 
 // getTweetIDFromURL()
-test.todo('get tweet id from url with http');
+test('get tweet id from url with http', (t) => {
+    const TWEET_ID = "798606989978910720";
+    const TWEET_URL = `http://twitter.com/freaktechnik/status/${TWEET_ID}`;
+
+    t.is(TwitterAccount.getTweetIDFromURL(TWEET_URL), TWEET_ID);
+});
+
 test('get tweet id from url with https', (t) => {
     const TWEET_ID = "798606989978910720";
     const TWEET_URL = `https://twitter.com/freaktechnik/status/${TWEET_ID}`;
 
     t.is(TwitterAccount.getTweetIDFromURL(TWEET_URL), TWEET_ID);
 });
-test.todo('get tweet id from url with http and www');
-test.todo('get tweet id from url with https and www');
-test.todo('don\'t get a tweet id from a non-tweet url');
+
+test('get tweet id from url with http and www', (t) => {
+    const TWEET_ID = "798606989978910720";
+    const TWEET_URL = `http://www.twitter.com/freaktechnik/status/${TWEET_ID}`;
+
+    t.is(TwitterAccount.getTweetIDFromURL(TWEET_URL), TWEET_ID);
+});
+
+test('get tweet id from url with https and www', (t) => {
+    const TWEET_ID = "798606989978910720";
+    const TWEET_URL = `https://www.twitter.com/freaktechnik/status/${TWEET_ID}`;
+
+    t.is(TwitterAccount.getTweetIDFromURL(TWEET_URL), TWEET_ID);
+});
+
+test('don\'t get a tweet id from a non-tweet url', (t) => {
+    t.is(TwitterAccount.getTweetIDFromURL('https://twitter.com/freaktechnik/status/824753300985769984/photo/1'), null);
+    t.is(TwitterAccount.getTweetIDFromURL('https://twitter.com/freaktechnik/status/'), null);
+    t.is(TwitterAccount.getTweetIDFromURL('https://example.com'), null);
+    t.is(TwitterAccount.getTweetIDFromURL('foo bart'), null);
+});
 
 // getRemainingChars()
 test.todo('remaining characters with a too long tweet');
