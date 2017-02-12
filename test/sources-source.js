91f080e7a0217d80849c9c3cde3f9e304e62b92a
Martin Giger <martin@humanoids.be>
2017-02-12 13:00:58 +0100

    add test for source

---
+++ test/sources-source.js
@@ -0,0 +1,16 @@
+import test from 'ava';
+import Source from '../lib/sources/source';
+
+test('constructor', (t) => {
+    const repo = 'a';
+    const twitterAccount = 'b';
+    const s = new Source(repo, twitterAccount);
+
+    t.is(s._repo, repo);
+    t.is(s._twitterAccount, twitterAccount);
+});
+
+test('required columns', (t) => {
+    t.true(Array.isArray(Source.requiredColumns));
+    t.is(Source.requiredColumns.length, 0);
+});
