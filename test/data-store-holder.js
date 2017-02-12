3f54dad4b5e113de5a206b7accd804e01df8a37f
Martin Giger <martin@humanoids.be>
2017-02-12 16:49:13 +0100

    Add tests for data-store-holder

--- test/data-store-holder.js
+++ test/data-store-holder.js
@@ -33,5 +33,46 @@ test('update updates data stores', async (t) => {
     t.true(updateStore.calledOnce);
 });
 
-test.todo('update fires updated event');
-test.todo('defined properties are magic stores that return a promise');
+test('update fires updated event', async (t) => {
+    const updateStore = sinon.spy(() => Promise.resolve());
+    const h = new DataStoreHolder({
+        store: updateStore
+    });
+
+    const promise = new Promise((resolve) => {
+        h.once('storesupdated', resolve);
+    });
+
+    h.update();
+
+    await promise;
+
+    t.true(updateStore.calledOnce);
+});
+
+test('defined properties are magic stores that return a promise', async (t) => {
+    const STORE_PROP = 'store';
+    const RV = 'a';
+    const updateStore = sinon.spy(() => Promise.resolve(RV));
+    const h = new DataStoreHolder({
+        [STORE_PROP]: updateStore
+    });
+
+    const returnVal1 = h[STORE_PROP];
+
+    t.is(typeof returnVal1.then, 'function');
+
+    const result1 = await returnVal1;
+
+    t.is(result1, RV);
+    t.true(updateStore.calledOnce);
+
+    const returnVal2 = h[STORE_PROP];
+
+    t.is(typeof returnVal2.then, 'function');
+
+    const result2 = await returnVal2;
+
+    t.is(result2, RV);
+    t.true(updateStore.calledOnce);
+});
