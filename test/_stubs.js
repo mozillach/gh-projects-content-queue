11a238924cf3154e229e8d0749c44e26011cb510
Martin Giger <martin@humanoids.be>
2017-10-18 21:32:42 +0200

    tests: Complete tweet card tests

--- test/_stubs.js
+++ test/_stubs.js
@@ -65,14 +65,14 @@ const getBoard = (columns) => ({
 });
 
 const getIssue = (content = 'lorem ipsum') => {
-    //TODO should use an actual issue instance instead with a no-op github client.
     const labels = new Set();
     return {
         content,
         addLabel: sinon.spy((label) => labels.add(label)),
         hasLabel: sinon.spy((label) => labels.has(label)),
         removeLabel: sinon.spy((label) => labels.delete(label)),
-        comment: sinon.spy()
+        comment: sinon.stub(),
+        assign: sinon.stub()
     };
 };
 
